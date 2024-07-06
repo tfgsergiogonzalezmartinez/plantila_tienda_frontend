649ae22a29cec8eb06205418a8044708ce52d6fc
sergio <sergio.gonzalez@tchlabs.eu>
2024-07-06 19:18:13 +0200

    fix interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { LoginLayoutComponent } from './Layout/LoginLayout/LoginLayout.component
 import { LoginPageComponent } from './Pages/LoginPage/LoginPage.component';
 import { MainPageComponent } from './Pages/MainPage/MainPage.component';
 import { UserProfileDropdownComponent } from './Components/UserProfileDropdown/UserProfileDropdown.component';
-import { ToggleModeComponent } from './Components/ToggleMode/ToggleMode.component';
 import { ItemComponent } from './Components/Item/Item.component';
 import { SubHeaderComponent } from './Components/SubHeader/SubHeader.component';
 import { CarritoComprasComponent } from './Components/CarritoCompras/CarritoCompras.component';
@@ -31,7 +30,6 @@ import { VentasPageComponent } from './Pages/VentasPage/VentasPage.component';
     LoginPageComponent,
     MainPageComponent,
     UserProfileDropdownComponent,
-    ToggleModeComponent,
     ItemComponent,
     SubHeaderComponent,
     CarritoComprasComponent,
